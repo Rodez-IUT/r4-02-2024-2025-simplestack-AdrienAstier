5abaa608e25fa4ecebfa960cbe9b3362c19c97e9
Adrien Astier <adrien.astier@iut-rodez.fr>
2025-01-22 10:31:36 +0100

    fix #2.3 refactoring effectué

--- src/main/java/org/example/SimpleStack.java
+++ src/main/java/org/example/SimpleStack.java
@@ -4,14 +4,21 @@ import java.util.ArrayList;
 
 public class SimpleStack implements Stack {
 
-    private ArrayList<Item> stack;
+    /**
+     * List of items, representing a stack
+     */
+    private final ArrayList<Item> stack;
 
+    /**
+     * Default constructor of the class
+     */
     public SimpleStack() {
         this.stack = new ArrayList<>();
     }
 
     /**
      * Tests if this stack is empty
+     * @return true if the stack is empty
      */
     @Override
     public boolean isEmpty() {
@@ -20,7 +27,7 @@ public class SimpleStack implements Stack {
     }
 
     /**
-     * Returns the number of items in this stack.
+     * @return the number of items in this stack.
      */
     @Override
     public int getSize() {
@@ -32,7 +39,7 @@ public class SimpleStack implements Stack {
      * Pushes an item onto the top of this stack.
      * null item is allowed.
      *
-     * @param item an item of the stack
+     * @param item the item to add in the stack
      */
     @Override
     public void push(Item item) {
@@ -41,6 +48,8 @@ public class SimpleStack implements Stack {
 
     /**
      * Looks at the object at the top of this stack without removing it from the stack.
+     * @return the item in the top of the stack
+     * @throws EmptyStackException if the stack is empty
      */
     @Override
     public Item peek() throws EmptyStackException {
@@ -55,12 +64,10 @@ public class SimpleStack implements Stack {
      * that object as the value of this function.
      *
      * @throws EmptyStackException if this stack is empty.
+     * @return the item that has been removed
      */
     @Override
     public Item pop() throws EmptyStackException {
-        if(this.isEmpty()) {
-            throw new EmptyStackException();
-        }
         Item itemPop = peek();
         this.stack.remove(peek());
         return itemPop;
